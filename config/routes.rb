3a35d784707889e1682ef397dc8f8c628d7a205d
MainFrame <munsonwf@gmail.com>
2015-05-04 22:39:48 -0400

    finishing user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
 
   resources :case_files
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
